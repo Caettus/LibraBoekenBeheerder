000e6ab0f502b428b5ae25fd00c12b02092774da
Caettus <vincentscheffer2@gmail.com>
2023-04-24 18:34:25 +0200

    GetCollectionsNotContainingBook Toegevoegd, nog niet getest of het werkt

--- LibraDB/BooksDAL.cs
+++ LibraDB/BooksDAL.cs
@@ -1,5 +1,6 @@
 using System.Data;
 using System.Data.SqlClient;
+using System.Runtime.Intrinsics.Arm;
 using Microsoft.Extensions.Configuration;
 
 namespace LibraDB;
@@ -17,102 +18,118 @@ public class BooksDAL
 
     }
     private SqlConnection con;
-        
-        private void connection()
+
+    private void connection()
+    {
+        string connstring = _configuration.GetConnectionString("MyConnectionString");
+        con = new SqlConnection(connstring);
+    }
+
+
+    public List<BooksDTO> GetAllBooks()
+    {
+        List<BooksDTO> mybooksList = new List<BooksDTO>();
+
+        connection();
+        SqlCommand cmd = new SqlCommand("SELECT * FROM dbo.Books", con);
+        cmd.CommandType = CommandType.Text;
+        con.Open();
+
+        SqlDataReader rdr = cmd.ExecuteReader();
+        while (rdr.Read())
         {
-            string connstring = _configuration.GetConnectionString("MyConnectionString");
-            con = new SqlConnection(connstring);
-        }
-        
+            var book = new BooksDTO();
 
-        public List<BooksDTO> GetAllBooks()
-        {
-            List<BooksDTO> mybooksList = new List<BooksDTO>();
+            book.BookId = Convert.ToInt32(rdr["BookId"]);
+            book.Title = rdr["title"].ToString();
+            book.Author = rdr["author"].ToString();
+            book.ISBNNumber = rdr["isbnnumber"].ToString();
+            book.Pages = Convert.ToInt32(rdr["pages"]);
+            book.PagesRead = Convert.ToInt32(rdr["pagesread"]);
+            book.Summary = rdr["summary"].ToString();
 
-            connection();
-            SqlCommand cmd = new SqlCommand("SELECT * FROM dbo.Books", con);
-            cmd.CommandType = CommandType.Text;
-            con.Open();
-
-            SqlDataReader rdr = cmd.ExecuteReader();
-            while (rdr.Read())
-            {
-                var book = new BooksDTO();
-
-                book.BookId = Convert.ToInt32(rdr["BookId"]);
-                book.Title = rdr["title"].ToString();
-                book.Author = rdr["author"].ToString();
-                book.ISBNNumber = rdr["isbnnumber"].ToString();
-                book.Pages = Convert.ToInt32(rdr["pages"]);
-                book.PagesRead = Convert.ToInt32(rdr["pagesread"]);
-                book.Summary = rdr["summary"].ToString();
-        
             if (book != null)
-                {
-                    mybooksList.Add(book);
-                }
-            }
-            con.Close();
-            return mybooksList;
-        }
-
-        public BooksDTO GetABook(int id)
-        {
-            connection();
-            SqlCommand cmd = new SqlCommand("SELECT * FROM dbo.Books WHERE BookId = @id", con);
-            cmd.Parameters.AddWithValue("@id", id);
-            cmd.CommandType = CommandType.Text;
-            con.Open();
-
-            SqlDataReader rdr = cmd.ExecuteReader();
-            BooksDTO book = null;
-            if (rdr.Read())
             {
-                book = new BooksDTO();
-                book.BookId = Convert.ToInt32(rdr["BookId"]);
-                book.Title = rdr["title"].ToString();
-                book.Author = rdr["author"].ToString();
-                book.ISBNNumber = rdr["isbnnumber"].ToString();
-                book.Pages = Convert.ToInt32(rdr["pages"]);
-                book.PagesRead = Convert.ToInt32(rdr["pagesread"]);
-                book.Summary = rdr["summary"].ToString();
+                mybooksList.Add(book);
             }
-            con.Close();
-            return book;
         }
+        con.Close();
+        return mybooksList;
+    }
 
-        
-        public bool CreateBook(BooksDTO booksDTO)
+    public BooksDTO GetABook(int id)
+    {
+        connection();
+        SqlCommand cmd = new SqlCommand("SELECT * FROM dbo.Books WHERE BookId = @id", con);
+        cmd.Parameters.AddWithValue("@id", id);
+        cmd.CommandType = CommandType.Text;
+        con.Open();
+
+        SqlDataReader rdr = cmd.ExecuteReader();
+        BooksDTO book = null;
+        if (rdr.Read())
         {
-            connection();
-            SqlCommand cmd = new SqlCommand("INSERT INTO [dbo].[Books] ([Title], [Author], [ISBNNumber], [Summary], [Pages], [PagesRead], [CollectionID]) VALUES (@Title, @Author, @ISBNNumber, @Summary, @Pages, @PagesRead, @CollectionID);", con);
-            cmd.CommandType = CommandType.Text;
-
-            cmd.Parameters.AddWithValue("@Title", booksDTO.Title);
-            cmd.Parameters.AddWithValue("@Author", booksDTO.Author);
-            cmd.Parameters.AddWithValue("@ISBNNumber", booksDTO.ISBNNumber);
-            cmd.Parameters.AddWithValue("@Summary", booksDTO.Summary);
-            cmd.Parameters.AddWithValue("@Pages", booksDTO.Pages);
-            cmd.Parameters.AddWithValue("@PagesRead", booksDTO.PagesRead);
-            cmd.Parameters.AddWithValue("@CollectionID", booksDTO.CollectionID);
-
-            con.Open();
-            var i = cmd.ExecuteNonQuery();
-            con.Close();
-
-            if (i >= 1)
-                return true;
-            else
-                return false;
+            book = new BooksDTO();
+            book.BookId = Convert.ToInt32(rdr["BookId"]);
+            book.Title = rdr["title"].ToString();
+            book.Author = rdr["author"].ToString();
+            book.ISBNNumber = rdr["isbnnumber"].ToString();
+            book.Pages = Convert.ToInt32(rdr["pages"]);
+            book.PagesRead = Convert.ToInt32(rdr["pagesread"]);
+            book.Summary = rdr["summary"].ToString();
         }
+        con.Close();
+        return book;
+    }
 
-        public BooksDTO GetCollectionsNotContainingBook(int id)
+
+    public bool CreateBook(BooksDTO booksDTO)
+    {
+        connection();
+        SqlCommand cmd = new SqlCommand("INSERT INTO [dbo].[Books] ([Title], [Author], [ISBNNumber], [Summary], [Pages], [PagesRead], [CollectionID]) VALUES (@Title, @Author, @ISBNNumber, @Summary, @Pages, @PagesRead, @CollectionID);", con);
+        cmd.CommandType = CommandType.Text;
+
+        cmd.Parameters.AddWithValue("@Title", booksDTO.Title);
+        cmd.Parameters.AddWithValue("@Author", booksDTO.Author);
+        cmd.Parameters.AddWithValue("@ISBNNumber", booksDTO.ISBNNumber);
+        cmd.Parameters.AddWithValue("@Summary", booksDTO.Summary);
+        cmd.Parameters.AddWithValue("@Pages", booksDTO.Pages);
+        cmd.Parameters.AddWithValue("@PagesRead", booksDTO.PagesRead);
+        cmd.Parameters.AddWithValue("@CollectionID", booksDTO.CollectionID);
+
+        con.Open();
+        var i = cmd.ExecuteNonQuery();
+        con.Close();
+
+        if (i >= 1)
+            return true;
+        else
+            return false;
+    }
+
+    public List<CollectionsDTO> GetCollectionsNotContainingBook(int id)
+    {
+        List<CollectionsDTO> collectionlist = new List<CollectionsDTO>();
+        connection();
+        SqlCommand cmd = new SqlCommand(
+                   "SELECT c.CollectionID, c.Name " +
+                   "FROM Collection c " +
+                   "LEFT JOIN CollectionBooks cb ON c.CollectionID = cb.CollectionID AND cb.BookId = @bookId " +
+                   "WHERE cb.BookId IS NULL", con);
+        cmd.Parameters.AddWithValue("@id", id);
+        cmd.CommandType = CommandType.Text;
+        con.Open();
+        SqlDataReader rdr = cmd.ExecuteReader();
+        while (rdr.Read())
         {
-            connection();
-            SqlCommand cmd = new SqlCommand(
-                "SELECT c.CollectionID, c.Name " +
-                       "FROM Collection c " +
-                       "LEFT JOIN CollectionBooks cb ON c.CollectionID = cb.CollectionID AND cb.BookId = @bookId " +
-                       "WHERE cb.BookId IS NULL");
+            var collection = new CollectionsDTO();
+
+            collection.CollectionsID = Convert.ToInt32(rdr["CollectionsID"]);
+            collection.Name = rdr["Name"].ToString();
+            collectionlist.Add(collection);
+
         }
+        con.Close();
+        return collectionlist;
+    }
 }
