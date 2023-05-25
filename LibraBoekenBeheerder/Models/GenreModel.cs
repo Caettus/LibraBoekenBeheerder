168b1bbe74dd07c50ff60606f017b1eacd4ae69f
Caettus <vincentscheffer2@gmail.com>
2023-05-25 19:32:24 +0200

    files for genre toegevoegd

---
+++ LibraBoekenBeheerder/Models/GenreModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraBoekenBeheerder.Models
+{
+    public class GenreModel
+    {
+        [Key]
+        public int GenreId { get; set; }
+
+        [StringLength(50)]
+        public string GenreName { get; set; }
+    }
+}
